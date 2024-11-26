c1b0c94d69981cbc755c0da80dff7050350682af
mayrars <mayra203@gmail.com>
2024-11-26 07:05:36 -0600

    Get products in home

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,19 +1,27 @@
-import { Card } from "flowbite-react";
+import { Card } from "flowbite-react"
 import { useFetch } from "../hooks/useFetch"
 
 export function Products() {
     const {data, loading} = useFetch("https://api.escuelajs.co/api/v1/products")
     return (
-        <div>
-            <h1>Products</h1>
+        <div className="container mx-auto mt-10">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-white">Products</h1>
             {loading && <p>Loading...</p>}
-            {data?.map((product) =>{
-                <Card>
-                    <div href="#" className="max-w-sm">
-                        {product.title}
-                    </div>
-                </Card>                 
+            <div className="grid grid-cols-4 gap-4">
+            {data?.map(product => {
+                return (
+                    <Card 
+                        key={product.id} 
+                        className="mt-10"
+                        renderImage={() => <img width={500} height={500} src={product.images} alt={product.title}/>}
+                    >
+                        <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{product.title}</h2>
+                        <p className="mt-2 text-sm text-gray-700 dark:text-gray-300">{product.description}</p>
+                        <p className="mt-2 text-4xl font-bold text-gray-700 dark:text-gray-300">${product.price}</p>
+                    </Card>
+                )
             })}
+            </div>
             
         </div>
     )
