7a6e4d305c45daa56262ea4d60e0b6c93ffcb914
mayrars <mayra203@gmail.com>
2024-11-28 06:53:13 -0600

    Created page categories

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,5 +1,15 @@
+import { useFetch } from "../hooks/useFetch"
 export default function Categories() {
+  const {data, loading} = useFetch("https://api.escuelajs.co/api/v1/categories")
   return <>
-    <h1>Categories</h1>
+    <div className="container mx-auto mt-10">
+      <h1 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-white mb-10">Categories</h1>
+      {loading && <p>Loading...</p>}
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {data && data.map(category => {
+          return category.name !== "New Category" ? <div key={category.id}><img src={category.image} /><h2 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-white mt-2">{category.name}</h2></div> : ''
+        })}
+      </div>
+    </div>
   </>
 }
