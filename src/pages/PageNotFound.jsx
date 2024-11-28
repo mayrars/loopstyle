1217a0f36a99d84236b2b5249b34b397fa656ac3
mayrars <mayra203@gmail.com>
2024-11-28 06:53:06 -0600

    Created page not found

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,7 @@
+export default function PageNotFound() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-white">Page not found</h1>
+    </div>
+  )
+}
