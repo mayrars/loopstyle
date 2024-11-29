45beb12e486b4abad2643a82e743aba56de0bd58
mayrars <mayra203@gmail.com>
2024-11-29 07:13:43 -0600

    Change card actaegories

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,13 +1,19 @@
 import { useFetch } from "../hooks/useFetch"
+import notAvailable from "../assets/Image_not_available.png"
+import {Card} from 'flowbite-react'
 export default function Categories() {
   const {data, loading} = useFetch("https://api.escuelajs.co/api/v1/categories")
   return <>
-    <div className="container mx-auto mt-10">
+    <div className="container mx-auto mt-10 mb-10">
       <h1 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-white mb-10">Categories</h1>
       {loading && <p>Loading...</p>}
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {data && data.map(category => {
-          return category.name !== "New Category" ? <div key={category.id}><img src={category.image} /><h2 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-white mt-2">{category.name}</h2></div> : ''
+          return category.name !== "New Category" ? 
+            <Card key={category.id}>
+              <img src={category.image=='https://placeimg.com/640/480/any' ? notAvailable : category.image} />
+              <h2 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-white mt-2">{category.name}</h2>
+            </Card> : ''
         })}
       </div>
     </div>
