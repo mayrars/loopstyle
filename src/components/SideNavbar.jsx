59f46c054d466e5ef32fd3788838fb2270bcaca1
mayrars <mayra203@gmail.com>
2024-12-11 07:08:22 -0600

    Adding key in item

--- src/components/SideNavbar.jsx
+++ src/components/SideNavbar.jsx
@@ -12,7 +12,7 @@ export default function SideNavbar({ cat }) {
           <Sidebar.ItemGroup>
             {data && data.map(category => {
               return category.name !== "New Category" ? 
-                <Sidebar.Item href={`/categories/${category.id}`}className={cat==category.id && 'bg-gray-200'}>{category.name}</Sidebar.Item>
+                <Sidebar.Item href={`/categories/${category.id}`}className={cat==category.id && 'bg-gray-200'} key={`Item-${category.id}`}>{category.name}</Sidebar.Item>
               : ''
             })}
             
