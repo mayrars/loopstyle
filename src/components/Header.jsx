e88c45c10af81a7037faa0a146a8b8350cfd5681
mayrars <mayra203@gmail.com>
2024-12-18 07:08:24 -0600

    import cart css and component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 "use client";
 import { Avatar, Dropdown, Navbar, Badge} from "flowbite-react";
 import { Link, NavLink } from "react-router-dom";
+import './Cart.css'
+import { Cart } from "./Cart";
 export function Header() {
     return (
         <Navbar className="bg-blue-950">
@@ -10,14 +12,7 @@ export function Header() {
             
             <div className="flex md:order-2 gap-5">
                 <div className="relative inline-flex items-center text-sm">
-                    <button type="button" className="relative rounded-full p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                        <span className="absolute -inset-1.5"></span>
-                        <span className="sr-only">View notifications</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-8">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-                        </svg>
-                    </button>
-                    <Badge className="absolute bottom-0 -right-2 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full">0</Badge>
+                    <Cart />
                 </div>
                 <Dropdown
                 arrowIcon={false}
