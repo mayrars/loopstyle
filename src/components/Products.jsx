91345be107eb5c830fd660d428916219785a6f7f
mayrars <mayra203@gmail.com>
2024-12-19 07:14:01 -0600

    import hook

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,8 +1,14 @@
 import { Spinner } from "flowbite-react"
 import { useFetch } from "../hooks/useFetch"
 import CardProduct from "../components/CardProduct"
+import { useCart } from '../hooks/useCart.jsx';
 
 export function Products() {
+    const {addToCart, cart} = useCart()
+    const checkProductInCart = product => {
+        return cart.some(item => item.id === product.id)
+    }
+    console.log(cart)
     const {data, loading} = useFetch("https://api.escuelajs.co/api/v1/products?offset=12&limit=12")
     return (
         <div className="container mx-auto mt-10">
@@ -10,8 +16,10 @@ export function Products() {
             {loading && <div className="flex justify-center items-center"><Spinner aria-label="Loading" size="xl" color="purple"/></div>}
             <div className="grid grid-cols-4 gap-4 mb-16">
             {data?.map(product => {
+                const isProductInCart = checkProductInCart(product)
+                console.log(isProductInCart)
                 return (
-                    <CardProduct key={`Product-${product.id}`} product={product} />                    
+                    <CardProduct key={`Product-${product.id}`} product={product} addToCart={addToCart} productInCart={isProductInCart} />                    
                 )
             })}
             </div>
