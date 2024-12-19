0a84f1e5d4d63e1a614681e45f0fb0aa05ba439d
mayrars <mayra203@gmail.com>
2024-12-19 07:13:08 -0600

    Import and envolvs with cart provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { createRoot } from 'react-dom/client'
 import { FiltersProvider } from './context/filters.jsx'
+import { CartProvider } from './context/cart.jsx'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <FiltersProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </FiltersProvider>
+  <CartProvider>
+    <FiltersProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </FiltersProvider>
+  </CartProvider>
 )
