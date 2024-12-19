3c82049863d5d0d215c5174c3f0dd1bfa81ba0a2
mayrars <mayra203@gmail.com>
2024-12-19 07:14:17 -0600

    add button to addcart

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -1,7 +1,8 @@
 import { Card,Button } from "flowbite-react"
 import { Link } from "react-router-dom"
 // eslint-disable-next-line react/prop-types
-export default function CardProduct({product}) {
+export default function CardProduct({product, addToCart, productInCart}) {
+    console.log(addToCart)
     // eslint-disable-next-line react/prop-types
     let image = product.images[0].replaceAll('["','').replaceAll('"]','')
     return (
@@ -17,8 +18,12 @@ export default function CardProduct({product}) {
                     <p><span className="text-sm">Price:</span></p>
                     <p className="mt-2 text-4xl font-bold text-gray-700 dark:text-gray-300">${product.price}</p>
                 </div>
-                <Button as={Link} to={`/product/${product.id}`} color="blue" pill size="lg" className="h-12">
-                    Read more
+                <Button size="sm" pill className="w-12 h-10" >
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#FFF" className="size-6">
+                        <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
+                    </svg>
+                </Button>
+                <Button as={Link} to={`/product/${product.id}`} color="blue" pill size="lg" className="h-12">Read more
                     <svg className="-mr-1 ml-2 h-5 w-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path
                         fillRule="evenodd"
