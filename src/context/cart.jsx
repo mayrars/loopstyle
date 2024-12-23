c77906ec108e8d3a8760099126ac0ad3a17d0052
mayrars <mayra203@gmail.com>
2024-12-22 19:04:44 -0600

    create remove from cart

--- src/context/cart.jsx
+++ src/context/cart.jsx
@@ -2,7 +2,7 @@ import { createContext, useState } from "react";
 
 export const CartContext = createContext();
 
-export const CartProvider = ({ children }) => {
+export function CartProvider ({ children }){
   const [cart, setCart] = useState([]);
 
   const addToCart = product => {    
@@ -21,12 +21,21 @@ export const CartProvider = ({ children }) => {
     ]))
   };
 
+  const removeFromCart = product => {
+    setCart(prevState => prevState.filter(item => item.id !== product.id));
+  }
+
   const clearCart = () => {
       setCart([]);
   }
 
   return (
-    <CartContext.Provider value={{ cart, addToCart, clearCart }}>
+    <CartContext.Provider value={{ 
+      cart, 
+      addToCart, 
+      removeFromCart,
+      clearCart 
+    }}>
       {children}
     </CartContext.Provider>
   )
