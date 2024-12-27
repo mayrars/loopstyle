b74763320fe19137ce6645538977fe864bfe171a
mayrars <mayra203@gmail.com>
2024-12-26 20:33:29 -0600

    Adding to localstorage for persistence of data

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,10 +1,16 @@
-export const cartInitialState = []
+export const cartInitialState = JSON.parse(window.localStorage.getItem('cart')) || []
 
 export const CART_ACTIONS_TYPES = {
     ADD_TO_CART: 'ADD_TO_CART',
     REMOVE_FROM_CART: 'REMOVE_FROM_CART',
     CLEAR_CART: 'CLEAR_CART'
 }
+
+//update local storage with state for cart
+export const updateLocalStorage = (state) => {
+    window.localStorage.setItem('cart', JSON.stringify(state))
+}
+
 
 export const cartReducer = (state, action) => {
     const {type: actionType, payload: actionPayload} = action
@@ -17,19 +23,24 @@ export const cartReducer = (state, action) => {
           newstate[productCartIndex].quantity += 1;
           return newstate;
         }
-        return [
+        const newState = [
           ...state,
           {
             ...actionPayload,
             quantity: 1
           }
         ]
+        updateLocalStorage(newState)
+        return newState
       }
       case CART_ACTIONS_TYPES.REMOVE_FROM_CART:{
         const {id} = actionPayload
-        return state.filter(item => item.id !== id)
+        const newState = state.filter(item => item.id !== id)
+        updateLocalStorage(newState)
+        return newState
       }
       case CART_ACTIONS_TYPES.CLEAR_CART:{
+        updateLocalStorage(cartInitialState)
         return cartInitialState
       }
     }
