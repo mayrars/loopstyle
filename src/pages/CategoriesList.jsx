d5b35f4c358cf3f36674b47d0cfc4410f89ce1e4
mayrars <mayra203@gmail.com>
2025-01-13 06:29:55 -0600

    removing sidenavbar

--- src/pages/CategoriesList.jsx
+++ src/pages/CategoriesList.jsx
@@ -1,10 +1,10 @@
 import { useParams } from "react-router-dom"
 import { useFetch } from "../hooks/useFetch"
 import { Spinner } from "flowbite-react"
-import SideNavbar from "../components/SideNavbar"
 import Filters from "../components/Filters"
 import {useFilters} from "../hooks/useFilters"
 import CardProduct from "../components/CardProduct"
+import SelectCategory from "../components/Select"
 
 export default function CategoriesList() {
   const params = useParams()  
@@ -12,13 +12,10 @@ export default function CategoriesList() {
   const {filterProducts} = useFilters(data)  
   return <>
     <div className="container mx-auto mt-10 mb-10">
-      <h1 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-white mb-10"></h1>
+      <h1 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-white mb-10">{data && data[0].category.name}</h1>
       {loading && <div className="flex justify-center items-center"><Spinner aria-label="Loading" size="xl" color="purple"/></div>}
       <div className="grid grid-cols-10 gap-4">
-        <div className="col-start-1 col-end-3">
-          <SideNavbar cat={params.id} />
-        </div>
-        <div className="col-start-3 col-end-11">
+        <div className="col-start-1 col-end-11">
           <Filters />
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
             {filterProducts && filterProducts.map(product => {
