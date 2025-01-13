7e22917230b978b82c65b314d5e93264f6a95ee6
mayrars <mayra203@gmail.com>
2025-01-13 05:58:58 -0600

    Change routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { lazy } from 'react';
+
 import './App.css'
 import { Header } from './components/Header';
 import { FooterPage } from './components/Footer'
@@ -5,9 +7,10 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Categories from './pages/Categories';
 import PageNotFound from './pages/PageNotFound';
-import CategoriesList from './pages/CategoriesList';
 import Product from './pages/Product';
 import { CartProvider } from './context/cart';
+const CategoriesList = lazy(() => import('./pages/CategoriesList'));
+import Contact from './pages/Contact';
 
 function App() {
 
@@ -16,10 +19,11 @@ function App() {
       <div className="wrapper">
         <Header />
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" index element={<Home />}></Route>
           <Route path="/product/:id" element={<Product />}></Route>
           <Route path="/categories" element={<Categories />}></Route>
-          <Route path="/categories/:id" element={<CategoriesList />}></Route>
+          <Route path="/products/:id" element={<CategoriesList />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
           <Route path="/*" element={<PageNotFound />}></Route>
         </Routes>
       </div>
