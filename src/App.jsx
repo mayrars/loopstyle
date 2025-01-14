1c89890183f95d5be797936ab86eb5b940ff54d0
mayrars <mayra203@gmail.com>
2025-01-14 07:10:47 -0600

    Adding login page

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Product from './pages/Product';
 import { CartProvider } from './context/cart';
 const CategoriesList = lazy(() => import('./pages/CategoriesList'));
 import Contact from './pages/Contact';
+import Login from './pages/Login';
 
 function App() {
 
@@ -24,6 +25,7 @@ function App() {
           <Route path="/categories" element={<Categories />}></Route>
           <Route path="/products/:id" element={<CategoriesList />}></Route>
           <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/login" element={<Login />}></Route>
           <Route path="/*" element={<PageNotFound />}></Route>
         </Routes>
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 "use client";
+import { useState } from "react";
 import { Avatar, Dropdown, Navbar} from "flowbite-react";
 import { Link, NavLink } from "react-router-dom";
 import './Cart.css'
 import { Cart } from "./Cart";
 export function Header() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const handleLoginLogout = () => {
+        setIsLoggedIn(!isLoggedIn); // Cambia el estado entre Login y Logout
+    };
     return (
         <Navbar className="bg-blue-950">
             <Link to={"/"} className="text-white active:text-blue-300">
@@ -14,23 +20,29 @@ export function Header() {
                 <div className="relative inline-flex items-center text-sm">
                     <Cart />
                 </div>
-                <Dropdown
-                arrowIcon={false}
-                inline
-                label={
-                    <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />
-                }
-                >
-                <Dropdown.Header>
-                    <span className="block text-sm">Bonnie Green</span>
-                    <span className="block truncate text-sm font-medium">name@flowbite.com</span>
-                </Dropdown.Header>
-                <Dropdown.Item>Dashboard</Dropdown.Item>
-                <Dropdown.Item>Settings</Dropdown.Item>
-                <Dropdown.Item>Earnings</Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item>Sign out</Dropdown.Item>
-                </Dropdown>
+                {isLoggedIn ? (
+                    <Dropdown
+                        arrowIcon={false}
+                        inline
+                        label={<Avatar alt="User settings" rounded />}
+                    >
+                        <Dropdown.Header>
+                            <span className="block text-sm">Bonnie Green</span>
+                            <span className="block truncate text-sm font-medium">name@flowbite.com</span>
+                        </Dropdown.Header>
+                        <Dropdown.Item>Dashboard</Dropdown.Item>
+                        <Dropdown.Item>Settings</Dropdown.Item>
+                        <Dropdown.Item>Earnings</Dropdown.Item>
+                        <Dropdown.Divider />
+                        <Dropdown.Item onClick={handleLoginLogout}>
+                            Logout
+                        </Dropdown.Item>
+                    </Dropdown>
+                ) : (
+                    <Link to={"/login"} className="text-white bg-blue-500 px-4 py-2 rounded-lg hover:bg-blue-600 transition duration-300">
+                        Login
+                    </Link>
+                )}
                 <Navbar.Toggle />
             </div>
             <Navbar.Collapse className="text-white">
